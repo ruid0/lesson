22f294152f16709229c92c392fe653d5e37a8662
Igor Bondarenko <noscream2@gmail.com>
2015-11-27 08:10:26 +0800

    lesson4

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,20 @@
-var React = require('react');
+var React = require('react'),
+  ReactDOM = require('react-dom');
 
 /*
-
   StorePicker commponent
-
  */
 
 var StorePicker = React.createClass({
   render: function() {
     return (
-      <p>Hello</p>
+      <form className="store-selector">
+      	<h2>Please enter a store</h2>
+      	<input type="text" ref="storeId"/>
+      	<input type="submit"/>
+      </form>
     )
   }
 });
 
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
