ac72fdab71205e3c08768a6c17dae66638e211e6
Igor Bondarenko <noscream2@gmail.com>
2015-11-27 08:30:23 +0800

    lesson6

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ var App = React.createClass({
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header/>
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
         <Inventory/>
@@ -26,7 +26,20 @@ var App = React.createClass({
 var Header = React.createClass({
   render: function() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          day
+        </h1>
+        <h3 className="tagline">
+          <span>
+            { this.props.tagline  }
+          </span>
+        </h3>
+      </header>
     )
   }
 });
